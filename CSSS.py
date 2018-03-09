92d600c7a624c7062946689741e7c0ce9675fe59
Michaelangelo David Tabone <mtabone@stanford.edu>
2018-03-09 14:01:12 -0800

    module created, added regularization

---
+++ CSSS.py
@@ -0,0 +1,149 @@
+import numpy as np
+import cvxpy as cvp
+
+class CSSS:
+### Contextually Supervised Source Seperation Class
+
+    def __init__(self, aggregateSignal):
+        self.aggregateSignal  = aggregateSignal
+        self.modelcounter     = 0   # Number of source signals
+        self.models           = {}  # Model for each source signal, this is a list but could be a dict
+        self.constraints      = []  # Additional constraints
+        self.N                = len(aggregateSignal) # Length of aggregate signal
+        
+
+    def addSource(self, regressor, name = None, 
+                  costFunction='sse',alpha = 1,      # Cost function for fit to regressors, alpha is a scalar multiplier
+                  regularizeTheta='None', beta = 1,  # Cost function for parameter regularization, beta is a scalar multiplier
+                  regularizeSource='None', gamma = 1 # Cost function for signal smoothing, gamma is a scalar multiplier
+                 ):
+        ### This is a method to add a new source
+        
+        ## TO DO 
+        # Regularize theta
+        # Regularize source signal
+        
+        self.modelcounter += 1   # Increment model counter
+        
+        ## Write model name if it doesn't exist. 
+        if name is None:
+            name = str(self.modelcounter)
+            
+        ## Instantiate a dictionary of model terms
+        model = {}
+        model['name'] = name
+        model['alpha'] = alpha
+        
+        ## Check regressor shape 
+        regressor = np.array(regressor)
+        if regressor.ndim == 0: ## If no regressors are included, set them an empty array
+            regressor = np.zeros((self.N,0))
+        if regressor.ndim == 1:
+            regressor = np.expand_dims(regressor,1)
+        if regressor.ndim > 2:
+            raise NameError('Regressors cannot have more than 2 dimensions')
+
+        
+        ## Check that regressors have the correct shape (Nobs, Nregressors)
+        if regressor.shape[0] != self.N:
+            if regressor.shape[1] == self.N:
+                regressor = regressor.transpose()
+            else:
+                raise NameError('Lengths of regressors and aggregate signal must match')
+            
+        ## Define model regressors and order
+        model['regressor'] = regressor
+        model['order']     = regressor.shape[1]
+        
+        ## Define decision variables and cost function style  
+        model['source']    = cvp.Variable(self.N,1)
+        model['theta']     = cvp.Variable(model['order'],1)
+        model['costFunction'] = costFunction
+        
+        ## Define objective function to fit model to regressors
+        if costFunction.lower() == 'sse':
+            residuals = (model['source'] - model['regressor'] * model['theta']) 
+            modelObj =  cvp.sum_squares(residuals) * model['alpha']
+            
+            
+        ## Define cost function to regularize theta ****************
+        # ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** *
+        # Check that beta is scalar or of length of number of parameters. 
+        beta = np.array(beta)
+        if beta.size not in [1, model['order']]:
+            raise NameError('beta must be scalar or vector with one element for each regressor')
+
+        if callable(regularizeTheta):
+            ## User can input their own function to regularize theta. 
+            # Must input a cvxpy variable vector and output a scalar
+            # or a vector with one element for each parameter.
+            
+            ## TODO: TRY CATCH TO ENSURE regularizeTheta WORKS AND RETURNS SCALAR
+            regThetaObj = regularizeTheta(model['theta']) * beta
+        elif regularizeTheta.lower() == 'sse':
+            ## Sum square errors. 
+            regThetaObj = cvp.sum_squares(model['theta'] * np.sqrt(beta)) 
+        elif regularizeTheta.lower() == 'l1':
+            regThetaObj = cvp.norm(model['theta'] * beta, 1) 
+        else:
+            print('Setting theta reg cost to 0')
+            regThetaObj = 0
+        ### ******* TO DO: ADD MORE MODEL FORMS. 
+            
+        ## Define cost function to regularize source signal ****************
+        # ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** *
+        # Check that gamma is scalar
+        gamma = np.array(gamma)
+        if gamma.size != 1:
+            raise NameError('gamma must be scalar')
+        
+        ## Calculate regularization.
+        if callable(regularizeSource):
+            ## User can input their own function to regularize the source signal. 
+            # Must input a cvxpy variable vector and output a scalar.
+            regSourceObj = regularizeSource(model['source']) * gamma
+        elif regularizeSource.lower() == 'diff1_sse':
+            regSourceObj = cvp.sum_squares(cvp.diff(model['source'])) * gamma
+        else: 
+            regSourceObj = 0
+        ### ******* TO DO: ADD MORE MODEL FORMS. 
+
+            
+        ## Sum total model objective
+        model['obj'] = modelObj + regThetaObj + regSourceObj
+        
+        ## Append model to models list
+        self.models[name]= model
+        return None
+    
+    def addConstraint(self, constraint):
+        ### This is a method to add a new source
+        self.constraints.append(constraint)
+        return None
+    
+    def constructSolve(self):
+        ## This method constructs and solves the optimization
+        
+        ## Initialize objective function and modeled aggregate signal as 0
+        obj = 0
+        sum_sources = np.zeros((self.N,1))
+        
+        ## Initialize constraints as those custom created
+        con = self.constraints
+        
+        ## For each model
+        #    - Add cost to objective function
+        #    - Add source to sum of sources
+        #    - (TODO) Add custom constraints for each source 
+        for name, model in self.models.items():
+            obj = obj + model['obj']
+            sum_sources = sum_sources + model['source']
+
+        ## Append the constraint that the sum of sources must equal aggergate signal
+        con.append(self.aggregateSignal == sum_sources)
+
+        ## Solve problem
+        prob = cvp.Problem(cvp.Minimize(obj), con)
+        prob.solve()
+        
+        return None
